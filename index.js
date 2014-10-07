eb7520f1ffce04dc07bb9b5ece094f6f9b508ec4
Justin Reynard <framerate@gmail.com>
2014-10-07 15:37:22 -0700

    Remove console/add comments

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var _ = require('underscore');
 var setupSocialSignin = require('./lib/setupSocialSignin');
 
 module.exports = function(app, config, loginUtils) {
-    // border-control currently expects an express instance
+    // border-control currently expects:
+    // app-  an express instance
+    // config - the border control config information for each site
+    // loginUtils - @TODO - this should not be here, since it's tied to karma
+    // It should just be two callback functions for approve/deny
     _.each(config, function(site){
-        console.log('BORDER-CONTROL ' + JSON.stringify(site, null, 4));
         var credentials = {};
         setupSocialSignin(app, site, loginUtils);
     });
