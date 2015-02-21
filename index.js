26860bcdf4387aec216a02df3f6a7e367fbd449a
Justin Reynard <framerate@gmail.com>
2015-02-20 16:04:45 -0800

    Updated border control to take specific callbacks
    One step more towards OSS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var _ = require('underscore');
 var borderControl = require('./lib/borderControl');
 
-module.exports = function(app, config, loginUtils) {
+module.exports = function(app, config, processLogin, processSuccessfulAuthorization) {
     // border-control currently expects:
     // app-  an express instance
     // config - the border control config information for each site
@@ -10,6 +10,6 @@ module.exports = function(app, config, loginUtils) {
     // It should just be two callback functions for approve/deny
     _.each(config, function(site){
         var credentials = {};
-        borderControl(app, site, loginUtils);
+        borderControl(app, site, processLogin, processSuccessfulAuthorization);
     });
 };
